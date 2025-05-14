aac5e697656045ba75e413b12a7ecd8167183ff5
Samuelfajnor <51970488+Samuelfajnor@users.noreply.github.com>
2025-05-14 17:33:30 +0200

    Update script-2025-grid-light-cursor.js

--- 2025-Grid-Light/script-2025-grid-light-cursor.js
+++ 2025-Grid-Light/script-2025-grid-light-cursor.js
@@ -1,1 +1,11 @@
+document.body.addEventListener("mousemove", evt => {
+  const mouseX = evt.clientX -15;
+  const mouseY = evt.clientY -30;
+
+  gsap.set(".cursor", {
+    x: mouseX,
+    y: mouseY });
+
+});
+
 
